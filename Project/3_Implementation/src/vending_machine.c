92e6649299f70b7f5122dd82a1d44bf3f2b280ca
99002666 <71492972+99002666@users.noreply.github.com>
2020-09-18 22:55:50 +0530

    Update vending_machine.c

--- Project/3_Implementation/src/vending_machine.c
+++ Project/3_Implementation/src/vending_machine.c
@@ -1,150 +1,72 @@
 #include "vending_machine.h"
 
 
-   void welcome_screen()
-   {
-        printf("\t\t\tWelcome to our Vending Machine!\n\n");
-        printf("\t\t\tCopyright Coca-Cola 2014 and 1356602\n\n");
-        printf("\t\t\tPress any key to continue.\n\n\n\n");
-       return;
-   }
-
-
-   void bill(float billmoney)
-   {
-       printf("Total amount to be payed is: %.2f\n\n\n",billmoney);
-       return;
-   }
-
-
-
-
-int penceentered()
-   {
-        int pence;
-        printf("Enter your money: \n");
-        printf("(5p 10p 20p 50p 100p)\n");
-        scanf("%d",&pence);
-
-
-       return pence;
-   }
-
-
-    int menu()
-    {
-  int selection;
-
-
-    printf("---------------------- \n");
-
-
-    printf("MENU: ");
-    printf("\n\n");
-    printf("1. Coca-Cola");            printf("\t\t\t£1.00\n");
-    printf("2. Sprite");               printf("\t\t\t£1.30\n");
-    printf("3. Fanta");                printf("\t\t\t£1.20\n");
-    printf("4. Dr. Pepper");           printf("\t\t\t£1.50\n");
-    printf("5. Quit");
-    printf("\n\n");
-
-
-    printf("Please enter your selection: ");
-    scanf("%d",&selection);
-
-
-    return selection;
-
-
-    }
-
-
-
-
-
-
-    void drinkchosen(int selection)
+  void menu(void)
 {
-switch(selection)
-{
-    case 1:
-printf("You have selected Coca-Cola.");            printf("\t\t\t£1.00\n");
-      break;
-   case 2:
-printf("You have selected Sprite.");               printf("\t\t\t£1.30\n");
-      break;
-   case 3:
-printf("You have selected Fanta.");                printf("\t\t\t£1.20\n");
-      break;
-   case 4:
-printf("You have selected Dr. Pepper.");           printf("\t\t\t£1.50\n");
-      break;
-   case 5:
-      break;
-   default:
-      printf("Invalid input!\n");
-      break;
-}
-return;
+  printf("*********************************************************************************************\n\n");
+  printf("\t\t\t|WELCOME TO CHOCOLATES VENDING MACHINE|\n\n");
+  printf("*********************************************************************************************\n");
+
+ printf("1.Select the Chocolates\n");
+ printf("2.Make payment\n");
+ printf("3.View payment bill\n");
+ printf("4.Exit Vending Machine\n");
+ printf("5.Exit Program\n\n");
 }
 
-
-
-
-    float cost(int selection)
- {
-  float price = 0;
-
-
-  switch(selection)
-  {
-     case 1:
-       price = 1.00;
-       break;
-     case 2:
-       price = 1.30;
-       break;
-     case 3:
-       price = 1.20;
-       break;
-    case 4:
-       price = 1.50;
-       break;
-   }
-   return price;
- }
-
-
-void wait(int seconds)
+void MakePayment()
 {
-    clock_t end_wait=(clock() + (seconds * CLOCKS_PER_SEC));
-    while(clock()<end_wait)
-    {}
+    int option;
+    printf("Select the Payment method\n");
+    printf("1.Cash\n");
+    printf("2.Credit/Debit card\n");
+    scanf("%d", &option);
+    if (option == 1)
+        PayByCash();
+    else
+        PayByCard();
 }
 
+void PayByCash()
+{
+   int money;
+   printf("Enter your money: ");
+   scanf("%d",&money);
+   printf("Your Balance: %d\n\n", money - payment);
+   printf("Please collect your cash\n\n");
+}
 
-void drinktotal(int selection,int amount)
-    {
-      switch(selection)
-      {
-      case 1:
-         printf("\n\n\t\t\tYou have selected %d Coca-Cola\n",amount);
-         printf("\t\t\t----------------------------\n");
-         break;
-      case 2:
-         printf("\n\n\t\t\tYou have chosen %d Sprite\n",amount);
-         printf("\t\t\t----------------------------\n");
-         break;
-      case 3:
-         printf("\n\n\t\t\tYou have chosen %d Fanta\n",amount);
-         printf("\t\t\t----------------------------\n");
-         break;
-      case 4:
-         printf("\n\n\t\t\tYou have chosen %d DR Pepper\n",amount);
-         printf("\t\t\t----------------------------\n");
-         break;
-      default:
-         printf("Invalid input!\n");
-         break;
-      }
- }
+void PayByCard()
+{
+   char pin[10];
+   printf("Slide your Debit/Credit card\n");
+   printf("Enter the 4-digit PIN\n");
+   scanf("%s", &pin);
+   if (strlen(pin) == 4)
+   {
+       printf("Payment made successfully..\n");
+       printf("Your Balance: %d\n\n\n", balance - payment);
+   }
+   else
+   {
+       printf("Invalid PIN, Try Again\n\n");
+   }
+}
+
+void chocolate(void)
+{
+ int u[30];
+ char o[30];
+ FILE *choclot;
+ choclot=fopen("parking.txt","a");
+ fprintf(choclot,"Chocolate: %s Total Price: %d\n\n",choco[choice - 1],payment);
+ fclose(choclot);
+}
+
+void end(void)
+{
+ system("cls");
+ printf("\t\t\tTHANK YOU FOR USING THIS SERVICES\n");
+  printf("\t\t\t\t  Visit Us At \n\n");
+  printf("\t\t\t    vendingmachine.com.my\n\n\n");
+} 
