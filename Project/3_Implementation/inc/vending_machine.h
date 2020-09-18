aeffec7eaa8ef78b1b5c1ef6be265d05ba16b5ee
99002666 <71492972+99002666@users.noreply.github.com>
2020-09-18 22:57:47 +0530

    Update vending_machine.h

--- Project/3_Implementation/inc/vending_machine.h
+++ Project/3_Implementation/inc/vending_machine.h
@@ -1,15 +1,16 @@
- #include <stdio.h>
-#include <curses.h>
-    #include <stdlib.h>
-    #include <time.h>
+#include<stdio.h>
+#include<string.h>
+#define system
+
+void menu(void);
+void chocolate(void);
+void MakePayment();
+void end(void);
+
+int payment;
+char choco[][10] = {"Cadburry Black Forest", "Kitkat", "Crispy", "Crunch", "Mars", "Kinder Bueno","White Kinder Bueno", "Milky Bar", "M&M", "Choki Choki"};
+int balance = 1000;
+int choice;
 
 
-    void wait(int seconds);
-    void welcome_screen();
-    void bill(float billmoney);
-    int penceentered();
-    int menu();
-    int differentcoins();
-    void drinktotal(int selection,int amount);
-    void  drinkchosen(int selection);
-    float cost(int selection);
+int choc[10]={0,1,2,3,4,5,6,7,8,9};
