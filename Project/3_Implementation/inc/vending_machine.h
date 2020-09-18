e57bae9dd0fb3b13c2bf9234df06f75ac180ae5a
99002666 <71492972+99002666@users.noreply.github.com>
2020-09-18 15:57:12 +0530

    Create vending_machine.h

---
+++ Project/3_Implementation/inc/vending_machine.h
@@ -0,0 +1,15 @@
+ #include <stdio.h>
+    #include <stdlib.h>
+    #include <conio.h>
+    #include <time.h>
+
+
+    void wait(int seconds);
+    void welcome_screen();
+    void bill(float billmoney);
+    int penceentered();
+    int menu();
+    int differentcoins();
+    void drinktotal(int selection,int amount);
+    void  drinkchosen(int selection);
+    float cost(int selection);
