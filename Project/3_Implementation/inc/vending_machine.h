e2d27ebd95d33b57579d7fd6d27487e9f7dd986a
99002666 <71492972+99002666@users.noreply.github.com>
2020-09-18 23:38:42 +0530

    Update vending_machine.h

--- Project/3_Implementation/inc/vending_machine.h
+++ Project/3_Implementation/inc/vending_machine.h
@@ -11,9 +11,6 @@ void MakePayment();
 void end(void);
 
 int payment;
-char choco[10][50] = {"Cadburry Black Forest", "Kitkat", "Crispy", "Crunch", "Mars", "Kinder Bueno","White Kinder Bueno", "Milky Bar", "M&M", "Choki Choki"};
-int balance = 1000;
 int choice;
-int choc[10]={0,1,2,3,4,5,6,7,8,9};
 
 #endif /* VENDING_H_ */
